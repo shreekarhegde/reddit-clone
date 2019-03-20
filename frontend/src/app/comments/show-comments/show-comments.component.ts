e0755e3f44f5d362291023bcdfaa541019734912
Shreekar Ganapati Hegde <shreekarhegde9@gmail.com>
2019-03-20 19:09:17 +0530

    feature/show-comments.component/attempt to fix ui of nested component

--- frontend/src/app/comments/show-comments/show-comments.component.ts
+++ frontend/src/app/comments/show-comments/show-comments.component.ts
@@ -15,7 +15,9 @@ export class ShowCommentsComponent implements OnInit {
   public headerParams;
   public firstLevelComments = [];
   public childComments;
-  @Input() comments;
+  // @Input() comments = [];
+  @Input() innerComments = [];
+  @Input() reOcurringCall = false;
 
   constructor(
     public httpService: HttpService,
@@ -25,6 +27,9 @@ export class ShowCommentsComponent implements OnInit {
   ) {}
 
   async ngOnInit() {
+    console.log('show-comments: this.reOcurringCall -->', this.reOcurringCall);
+    if (this.reOcurringCall) return;
+
     this.headerParams = await this.tokenService.checkTokenAndSetHeader();
 
     //get post id sent from display post component
@@ -41,35 +46,37 @@ export class ShowCommentsComponent implements OnInit {
     let query = `?postID=${this.postID}&$populate=userID`;
 
     //get all comments and check for first level comments. If parentCommentID is null, find their child comments.
-    // this.httpService.getRequest(`${this.commentsUrl}` + query, this.headerParams).subscribe(
-    //   async comments => {
-    //     console.log('show-comments: all comments------->', comments);
-    //     if (comments) {
-    //       for (let i = 0; i < comments['data'].length; i++) {
-    //         console.log(comments['data'][i].hasOwnProperty('parentCommentID'));
-    //         if (!comments['data'][i].hasOwnProperty('parentCommentID')) {
-    //           console.log('first level comments-------->', comments['data'][i]);
-    //           let query = '/?parentCommentID=' + comments['data'][i]['_id'];
+    this.httpService.getRequest(`${this.commentsUrl}` + query, this.headerParams).subscribe(
+      async comments => {
+        console.log('show-comments: all comments------->', comments);
+        if (comments) {
+          for (let i = 0; i < comments['data'].length; i++) {
+            console.log(comments['data'][i].hasOwnProperty('parentCommentID'));
+            if (!comments['data'][i].hasOwnProperty('parentCommentID')) {
+              console.log('first level comments-------->', comments['data'][i]);
+              let query = '/?parentCommentID=' + comments['data'][i]['_id'];
 
-    //           let commentsResponse = await this.httpService.getRequest('http://localhost:3030/child-comments' + query, this.headerParams);
+              let commentsResponse = await this.httpService.getRequest('http://localhost:3030/child-comments' + query, this.headerParams);
 
-    //           commentsResponse.subscribe(
-    //             async res => {
-    //               if (res['data']) {
-    //                 console.log('res.data----------->', res['data']);
-    //               }
-    //             },
-    //             err => {
-    //               console.log(err);
-    //             }
-    //           );
-    //         }
-    //       }
-    //     }
-    //   },
-    //   err => {
-    //     console.log(err);
-    //   }
-    // );
+              commentsResponse.subscribe(
+                async res => {
+                  if (res['data']) {
+                    console.log('res.data----------->', res['data']);
+                    this.firstLevelComments.push({ self: comments['data'][i], innerComments: res['data'] });
+                    // this.innerComments = res['data'];
+                  }
+                },
+                err => {
+                  console.log(err);
+                }
+              );
+            }
+          }
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 }
--- frontend/src/app/navigation/top-navigation/top-navigation.component.ts
+++ frontend/src/app/navigation/top-navigation/top-navigation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
+import { TokenService } from 'src/app/services/token.service';
+import { UserDetailsService } from 'src/app/services/user-details.service';
 
 @Component({
   selector: 'app-top-navigation',
@@ -6,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-navigation.component.css']
 })
 export class TopNavigationComponent implements OnInit {
-  constructor() {}
+  public userName;
+  constructor(public httpService: HttpService, public tokenService: TokenService, public userDetailsService: UserDetailsService) {}
 
-  ngOnInit() {}
+  async ngOnInit() {
+    let usersUrl = 'http://localhost:3030/users';
+    let headerParams = await this.tokenService.checkTokenAndSetHeader();
+    let userID = this.userDetailsService.getUserID();
+    this.httpService.getRequest(`${usersUrl}/${userID}`, headerParams).subscribe(res => {
+      this.userName = res['username'];
+    });
+  }
 }

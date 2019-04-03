e8267e1d9930e8a3cb972c9699be969c426a9e53
Shreekar Ganapati Hegde <shreekarhegde9@gmail.com>
2019-04-03 15:47:42 +0530

    feature/top-navigation.component/navigate to profile from dropdown

--- frontend/src/app/navigation/top-navigation/top-navigation.component.ts
+++ frontend/src/app/navigation/top-navigation/top-navigation.component.ts
@@ -3,6 +3,7 @@ import { HttpService } from 'src/app/services/http.service';
 import { TokenService } from 'src/app/services/token.service';
 import { UserDetailsService } from 'src/app/services/user-details.service';
 import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-navigation',
@@ -11,18 +12,20 @@ import { MatSnackBar } from '@angular/material';
 })
 export class TopNavigationComponent implements OnInit {
   public userName: string = '';
+  public userID: any = '';
   constructor(
     public httpService: HttpService,
     public tokenService: TokenService,
     public userDetailsService: UserDetailsService,
-    public snackbar: MatSnackBar
+    public snackbar: MatSnackBar,
+    public router: Router
   ) {}
 
   async ngOnInit() {
     let usersUrl = 'http://localhost:3030/users';
     let headerParams = this.tokenService.checkTokenAndSetHeader();
-    let userID = await this.userDetailsService.getUserID();
-    this.httpService.getRequest(`${usersUrl}/${userID}`, headerParams).subscribe(
+    this.userID = await this.userDetailsService.getUserID();
+    this.httpService.getRequest(`${usersUrl}/${this.userID}`, headerParams).subscribe(
       res => {
         this.userName = res['username'];
       },
@@ -30,6 +33,12 @@ export class TopNavigationComponent implements OnInit {
         this.showNotification(err, 'err', 'could not receive user name');
       }
     );
+  }
+
+  onChange(event) {
+    if (event['target']['value'] === 'My profile') {
+      this.router.navigate(['', 'users', this.userID, 'comments']);
+    }
   }
 
   showNotification(err, type, message) {

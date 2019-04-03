59d051d8728212a75c29f91fedeabdb693fc719c
Shreekar Ganapati Hegde <shreekarhegde9@gmail.com>
2019-04-03 13:55:28 +0530

    feature/user-register.component.ts,user-register.component.html/validation for password

--- frontend/src/app/user/user-register/user-register.component.ts
+++ frontend/src/app/user/user-register/user-register.component.ts
@@ -14,9 +14,11 @@ export class UserRegisterComponent implements OnInit {
   constructor(public http: HttpService, public router: Router, public tokenService: TokenService, public snackbar: MatSnackBar) {}
   public isNextButtonClicked: boolean = false;
   public email: string = '';
-  public emailPattern = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$');
-  public username: string = '';
   public password: string = '';
+
+  public emailPattern = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$');
+  public passwordPattern = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$');
+  public username: string = '';
   public usersUrl: string = 'http://localhost:3030/users';
   public authUrl: string = 'http://localhost:3030/authentication';
 
@@ -24,38 +26,47 @@ export class UserRegisterComponent implements OnInit {
 
   getUserEmail(f: NgForm): void {
     this.email = f.value.email ? f.value.email : null;
-    console.log(f.value.email)
-    if(this.emailPattern.test(this.email)){
+    console.log(f.value.email);
+    if (this.emailPattern.test(this.email)) {
       this.isNextButtonClicked = true;
     }
   }
 
-  async sendUserDetails() {
-    let userDetails = {
-      email: this.email,
-      username: this.username,
-      password: this.password
-    };
+  sendUserDetails(f: NgForm) {
+    let userDetails = {};
+    this.username = f.value.username;
+    this.password = f.value.password;
+    console.log(this.password);
 
-    this.http.postRequest(this.usersUrl, userDetails, null).subscribe(
-      user => {
-        console.log('post: user-------->', user);
-        this.http
-          .postRequest(this.authUrl, { strategy: 'local', username: this.username, password: this.password }, null)
-          .subscribe(res => {
-            if (res) {
-              console.log('res from register', res);
-              this.tokenService.setToken(res);
-              this.router.navigateByUrl('r');
-            }
-          });
-      },
-      err => {
-        if (err['statusText'] === 'Conflict') {
-          this.showNotification(err, 'err', 'This user name is already taken. Please choose a different username.');
+    console.log(this.passwordPattern.test(this.password));
+    if (this.passwordPattern.test(this.password)) {
+      userDetails = {
+        email: this.email,
+        username: this.username,
+        password: this.password
+      };
+      console.log(userDetails);
+
+      this.http.postRequest(this.usersUrl, userDetails, null).subscribe(
+        user => {
+          console.log('post: user-------->', user);
+          this.http
+            .postRequest(this.authUrl, { strategy: 'local', username: this.username, password: this.password }, null)
+            .subscribe(res => {
+              if (res) {
+                console.log('res from register', res);
+                this.tokenService.setToken(res);
+                this.router.navigateByUrl('r');
+              }
+            });
+        },
+        err => {
+          if (err['statusText'] === 'Conflict') {
+            this.showNotification(err, 'err', 'This user name is already taken. Please choose a different username.');
+          }
         }
-      }
-    );
+      );
+    }
   }
 
   backToEmail() {

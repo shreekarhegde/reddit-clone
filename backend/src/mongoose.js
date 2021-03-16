8187ddceb203709fb9e9f27b53c45100985624e7
Shreekar Ganapati Hegde <shreekarhegde9@gmail.com>
2021-03-16 21:40:10 +0530

    Added mongo atlas url

--- backend/src/mongoose.js
+++ backend/src/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 module.exports = function (app) {
   mongoose.connect(
-    app.get('mongodb'),
+    process.env.MONGO_URL,
     { useCreateIndex: true, useNewUrlParser: true }
   );
   mongoose.Promise = global.Promise;
